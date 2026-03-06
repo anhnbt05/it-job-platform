6482bf146507cc7119b7bca4a9b8dfb066e3d983
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2026-03-06 23:34:22 +0700

    feat: add getMe API and redis infrastructure

--- services/identity-service/src/modules/users/users.service.ts
+++ services/identity-service/src/modules/users/users.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { RoleEnum } from '@/common/enums';
+import { TUserSession } from '@/common/types';
+import { PrismaService } from '@/modules/prisma/prisma.service';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { omit } from 'lodash';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getMe(userSession: TUserSession) {
+    const { id, role } = userSession;
+
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        ...(role === RoleEnum.CANDIDATE && {
+          candidate: true,
+        }),
+        ...(role === RoleEnum.RECRUITER && {
+          recruiter: {
+            include: {
+              company: true,
+              branch: true,
+            },
+          },
+        }),
+        profile: true,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Không tìm thấy thông tin của bạn.');
+    }
+
+    return omit(user, ['password']);
+  }
+}
