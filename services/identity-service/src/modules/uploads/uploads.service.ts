7e533fa95ce4086cd1321ec4dff418fdfde11f7d
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2026-03-30 20:50:26 +0700

    feat(upload): add upload avatar and resume  feature

--- services/identity-service/src/modules/uploads/uploads.service.ts
+++ services/identity-service/src/modules/uploads/uploads.service.ts
@@ -1,5 +1,10 @@
-import { UploadedImage } from '@/common/types';
-import { Injectable } from '@nestjs/common';
+import { TUserSession, UploadedImage } from '@/common/types';
+import { PrismaService } from '@/modules/prisma/prisma.service';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import ImageKit from 'imagekit';
 
@@ -7,7 +12,10 @@ import ImageKit from 'imagekit';
 export class UploadsService {
   private readonly imagekit: ImageKit;
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly prismaService: PrismaService,
+  ) {
     this.imagekit = new ImageKit({
       publicKey: this.configService.get<string>('imagekit.public_key') ?? '',
       privateKey: this.configService.get<string>('imagekit.private_key') ?? '',
@@ -37,6 +45,40 @@ export class UploadsService {
       width: uploadResult.width,
       size: uploadResult.size,
       fileType: uploadResult.fileType,
+    };
+  }
+
+  async uploadResume(file: Express.Multer.File, userSession: TUserSession) {
+    const { id } = userSession;
+    const { url } = await this.uploadImage(file);
+
+    if (!url?.trim()) {
+      throw new InternalServerErrorException('Đã xảy ra lỗi khi upload CV.');
+    }
+
+    const candidateInfo = await this.prismaService.candidate.findUnique({
+      where: {
+        user_id: id,
+      },
+    });
+
+    if (!candidateInfo) {
+      throw new NotFoundException('Không tìm thấy thông tin ứng viên của bạn.');
+    }
+
+    await this.prismaService.candidate.update({
+      where: { user_id: id },
+      data: {
+        resume_urls: [...candidateInfo.resume_urls, url],
+      },
+    });
+
+    return {
+      success: true,
+      mesasge: 'Uploaded CV successfully.',
+      data: {
+        resumeUrl: url,
+      },
     };
   }
 }
